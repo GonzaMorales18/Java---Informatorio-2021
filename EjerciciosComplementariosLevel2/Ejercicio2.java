74286fd1efdb38ed374cbfda3ffc74924d799f98
Gonzalo Morales <gonzalo.morales@hotmail.com.ar>
2021-10-12 00:07:30 -0300

    Carga

--- EjerciciosComplementariosLevel2/Ejercicio2.java
+++ EjerciciosComplementariosLevel2/Ejercicio2.java
@@ -14,10 +14,31 @@ public class Ejercicio2 {
             int num = input.nextInt();
             numeros.add(num);
             cont = cont + 1;
+        }
+        System.out.println("Ingrese el primer número a agregar: ");
+        int num1 = input.nextInt();
+        numeros.add(0, num1);
+        System.out.println("Ingrese el útimo número a agregar: ");
+        int num2 = input.nextInt();
+        numeros.add(num2);
+        MostrarLista(numeros);
+        input.close();
     }
 
-    public static void AgregarALista(){
+    public static void MostrarLista(List<Integer> x){
+        System.out.println("Elementos de la lista modificada: " + x.size());
+        System.out.print("Elementos: " );
+        for (int i:x){
+            System.out.print(i + ", ");
+        }
 
-    
+        x.remove(0);
+        x.remove(x.size()-1);
+
+        System.out.println("\nElementos de la lista sin modificar: " + x.size());
+        System.out.print("Elementos: " );
+        for (int i:x){
+            System.out.print(i + ", ");
+        }
     }
 }
