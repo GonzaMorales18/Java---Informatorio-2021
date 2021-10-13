1d0d164e9b95a5cd34827842208e0966f48ced4a
Gonzalo Morales <gonzalo.morales@hotmail.com.ar>
2021-10-13 03:10:52 -0300

    Carga

--- EjerciciosComplementariosLevel2/Ejercicio7.java
+++ EjerciciosComplementariosLevel2/Ejercicio7.java
@@ -5,8 +5,11 @@ import java.util.*;
 public class Ejercicio7 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        List<String> lista = fizzBuzzFuncion(1,6); // Profe, no encuentro la manera de ingresar con el Scanner una función, así que lo hice así :(
-        System.out.println(Arrays.toString(lista.toArray()));
+        System.out.println("Ingrese el primer número: ");
+        int num1 = input.nextInt();
+        System.out.println("Ingrese el segundo número (mayor que el primero): ");
+        int num2 = input.nextInt();
+        System.out.println(Arrays.toString(fizzBuzzFuncion(num1,num2).toArray()));
 
         input.close();
     }
